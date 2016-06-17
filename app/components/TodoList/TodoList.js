cbd6bc9ebb238edf3115095696e9cde892e154b4
mmcfeng <xifeng.zhang@360>
2016-06-17 18:17:54 +0800

    ce

--- app/components/TodoList/TodoList.js
+++ app/components/TodoList/TodoList.js
@@ -1,19 +1,36 @@
-import $ from 'jquery';
 import './TodoList.css';
 import React from 'react';
+import uid from 'node-uuid';
 
 export default class TodoList extends React.Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {
+          notes:[
+              {id:uid.v4(),text:'list one'},
+              {id:uid.v4(),text:'list two'},
+              {id:uid.v4(),text:'list three'},
+              {id:uid.v4(),text:'list four'},
+              {id:uid.v4(),text:'list five'}
+          ] 
+        }
+    }
+
+    addNote(){
+        this.setState({notes:this.state.notes.push({id:uid.v4(),text:'add new'})})
+    }
 
     render() {
         return (
-          <ul className="todo-list">
-            <li>list one</li>
-            <li>list one</li>
-            <li>list one</li>
-            <li>list one</li>
-            <li>list one</li>
-            <li>list one</li>
-          </ul>
+          <div>
+            <button onClick={()=>{this.addNote()}}>add</button>
+            <ul className="todo-list">
+              {this.state.notes.map(note=>{
+                  return <li key={note.id}>{note.text}</li>
+              })}
+            </ul>
+          </div>
         )
     }
 }
